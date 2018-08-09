ce1bb6a9c335690012da0453ca6c33124fea1a33
tomoncle <1123431949@qq.com>
2018-08-09 09:34:07 +0800

    .

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='prometheus-http-client',
-    version='0.0.1',
-    author='tomoncle',
+    version='1.0.0',
+    author='liyuanjun',
     author_email='1123431949@qq.com',
-    url='https://github.com/tomoncle/prometheus-client',
-    description='Prometheus service http client, author tomoncle',
+    url='https://github.com/tomoncle/prometheus-http-client',
+    description='Prometheus service http client, author liyuanjun',
     long_description=open("README.md").read(),
     license="MIT",
     install_requires=[
